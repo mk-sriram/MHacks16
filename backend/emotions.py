31a44b963f9d6f971f462696818b234c510e1208
Sid Bhattacharya <bhatta70@msu.edu>
2023-11-18 15:17:28 -0500

    Update emotions.py

--- backend/emotions.py
+++ backend/emotions.py
@@ -1,13 +1,26 @@
 from google.cloud import vision
-
-def get_emotion(image_path):
-    client = vision.ImageAnnotatorClient.from_service_account_json('key.json')
-    with open(image_path, 'rb') as image_file:
+def detect_faces(path):
+    """Detects faces in an image."""
+    client = vision.ImageAnnotatorClient.from_service_account_json('keys.json')
+    with open(path, "rb") as image_file:
         content = image_file.read()
     image = vision.Image(content=content)
     response = client.face_detection(image=image)
     faces = response.face_annotations
     if len(faces) == 0:
-        return 0
+        return None
     face = faces[0]
-    return face.anger_likelihood
+
+    emotions = {'joy': face.joy_likelihood,
+               'anger': face.anger_likelihood,
+               'sorrow': face.sorrow_likelihood,
+               'surprise': face.surprise_likelihood}
+    
+    # return highest likelihood emotion
+    emotion = None
+    max_likelihood = 0
+    for key in emotions:
+        if emotions[key] > max_likelihood:
+            max_likelihood = emotions[key].value
+            emotion = key
+    return emotion, max_likelihood
